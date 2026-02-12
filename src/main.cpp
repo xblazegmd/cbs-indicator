87a82773af8cc7fa415b396d77dafdab22aa85c8
xblazegmd <santiagopalaciocolina7@gmail.com>
2026-02-12 12:55:50 -0500

    Add settings

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,54 @@
 #include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/EndLevelLayer.hpp>
 
+#include <cstdint>
+#include <string>
+
 using namespace geode::prelude;
 
 // For EndLevelLayer
 static bool g_clickBetweenSteps = false;
 static bool g_clickOnSteps = false;
 
+static Mod* g_mod = Mod::get();
+
+void setPositionBasedOnSetting(CCNode* node, const std::string& setting) {
+    auto winSize = CCDirector::sharedDirector()->getWinSize();
+    std::string alignment = g_mod->getSettingValue<std::string>(setting);
+
+    if (alignment == "Top-Left") {
+        node->setPosition({ 0, winSize.height });
+        node->setAnchorPoint({ 0, 1 });
+    } else if (alignment == "Top-Right") {
+        node->setPosition({ winSize.width, winSize.height });
+        node->setAnchorPoint({ 1, 1 });
+    } else if (alignment == "Bottom-Left") {
+        node->setPosition({ 0, 0 });
+        node->setAnchorPoint({ 0, 0 });
+    } else if (alignment == "Bottom-Right") {
+        node->setPosition({ winSize.width, 0 });
+        node->setAnchorPoint({ 1, 0 });
+    }
+}
+
 class $modify(CBSPlayLayer, PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
         if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
+        if (!g_mod->getSettingValue<bool>("gp-enabled")) return true;
 
         // Indicator
         std::string indText;
+
+        // Since CBS and CoS can be enabled at the same time
         if (m_clickOnSteps && !m_clickBetweenSteps) indText = "CoS";
         else if (m_clickBetweenSteps) indText = "CBS";
 
         auto indicator = CCLabelBMFont::create(indText.c_str(), "bigFont.fnt");
         indicator->setVisible(m_clickBetweenSteps || m_clickOnSteps);
         indicator->setScale(.2f);
-        indicator->setOpacity(10);
+        indicator->setOpacity(g_mod->getSettingValue<int64_t>("gp-opacity"));
 
-        auto winSize = CCDirector::sharedDirector()->getWinSize();
-        indicator->setPosition({ 0, winSize.height });
-        indicator->setAnchorPoint({ 0, 1 });
+        setPositionBasedOnSetting(indicator, "gp-position");
 
         indicator->setID("indicator"_spr);
 
@@ -37,7 +62,7 @@ class $modify(CBSPlayLayer, PlayLayer) {
     void levelComplete() {
         PlayLayer::levelComplete();
         g_clickBetweenSteps = m_clickBetweenSteps;
-        g_clickOnSteps = m_clickOnSteps && !m_clickBetweenSteps;
+        g_clickOnSteps = m_clickOnSteps && !m_clickBetweenSteps; // Handle the "CBS and CoS at the same time" case
 
         // Fade out
         auto indicator = m_uiLayer->getChildByID("indicator"_spr);
@@ -50,7 +75,7 @@ class $modify(CBSPlayLayer, PlayLayer) {
         // Restore opacity after fade out
         auto indicator = typeinfo_cast<CCLabelBMFont*>(m_uiLayer->getChildByID("indicator"_spr));
         if (!indicator) return;
-        indicator->setOpacity(10);
+        indicator->setOpacity(g_mod->getSettingValue<int64_t>("gp-opacity"));
     }
 };
 
@@ -59,27 +84,26 @@ class $modify(CBSEndLevelLayer, EndLevelLayer) {
         EndLevelLayer::customSetup();
         if (!g_clickBetweenSteps && !g_clickOnSteps) return;
 
-        // Watermark on the top-right
+        // Watermark
         std::string watermarkText;
         if (g_clickOnSteps) watermarkText = "CoS";
         else if (g_clickBetweenSteps) watermarkText = "CBS";
 
         auto watermark = CCLabelBMFont::create(watermarkText.c_str(), "bigFont.fnt");
-        watermark->setVisible(g_clickBetweenSteps || g_clickOnSteps);
+        watermark->setVisible((g_clickBetweenSteps || g_clickOnSteps) && g_mod->getSettingValue<bool>("wm-enabled"));
         watermark->setScale(.2f);
         watermark->setOpacity(10);
 
-        auto winSize = CCDirector::sharedDirector()->getWinSize();
-        watermark->setPosition({ winSize.width, winSize.height });
-        watermark->setAnchorPoint({ 1, 1 });
+        setPositionBasedOnSetting(watermark, "wm-position");
 
         watermark->setID("watermark"_spr);
         this->addChild(watermark);
 
         // Custom completion text
+        if (!g_mod->getSettingValue<bool>("end-text-enabled")) return;
         std::string completionStr;
-        if (g_clickOnSteps) completionStr = "CoS";
-        else if (g_clickBetweenSteps) completionStr = "CBS Detected Loser!";
+        if (g_clickOnSteps) completionStr = g_mod->getSettingValue<std::string>("end-text-cos");
+        else if (g_clickBetweenSteps) completionStr = g_mod->getSettingValue<std::string>("end-text-cbs");
 
         // Temporary until NodeIDs works again
         for (auto child : CCArrayExt<CCNode*>(m_mainLayer->getChildren()))
